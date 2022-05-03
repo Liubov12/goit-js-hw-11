0033c719f32169c96d2a4571a30bf4faa5b75d67
Liubov12 <68028382+Liubov12@users.noreply.github.com>
2022-05-03 08:40:55 +0200

    ready_hw

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export default  {
+    form : document.querySelector('search-form'),
+    input: document.querySelector('input[name="searchQuery"]'),
+    gallery : document.querySelector('.gallery'),
+    btnSubmit: document.querySelector('button'),
+    body: document.querySelector('body'),
+    loadMoreBtn: document.querySelector('.loadMore')
+  };
