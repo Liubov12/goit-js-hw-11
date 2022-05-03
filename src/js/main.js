16e823b86056ca74022ea2353d2ad5dd8169ede4
Liubov12 <68028382+Liubov12@users.noreply.github.com>
2022-05-03 22:11:40 +0200

    formate code with prettier

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,19 @@
 import '../sass/main.scss';
-'use strict';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ImagesAPIService from './apiService';
 import LoadMoreBtn from './loadMore';
 import Markup from './markupTemplate';
 
 const refs = {
-    form: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery'),
-  };
-  
-  const imagesAPIService = new ImagesAPIService();
-  const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more' });
-  const renderMarkup = new Markup({ selector: refs.gallery });
-  
-  refs.form.addEventListener('submit', onFormSubmit);
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
+
+const imagesAPIService = new ImagesAPIService();
+const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more' });
+const renderMarkup = new Markup({ selector: refs.gallery });
+
+refs.form.addEventListener('submit', onFormSubmit);
 loadMoreBtn.button.addEventListener('click', onloadMoreBtnClick);
 
 // Submit handler
@@ -43,42 +42,40 @@ async function onFormSubmit(e) {
 }
 // Load-More Button handler
 async function onloadMoreBtnClick() {
-    try {
-      await initFetchImages();
-    } catch {
-      Notify.failure(error.message);
-    }
-    pageScroll();
-    renderMarkup.lightbox.refresh();
+  try {
+    await initFetchImages();
+  } catch {
+    Notify.failure(error.message);
   }
-  
-  // Send request
-  async function initFetchImages() {
-    
-    try {
-      loadMoreBtn.disable();
-      const images = await imagesAPIService.fetchImages();
-      renderMarkup.items = images;
-      renderMarkup.render();
-    } catch {
-      Notify.failure(error.message);
-    }
-     
-  
-    if (imagesAPIService.endOfHits) {
-      loadMoreBtn.hideBtn();
-      return;
-    }
-    loadMoreBtn.enable();
+  pageScroll();
+  renderMarkup.lightbox.refresh();
+}
+
+// Send request
+async function initFetchImages() {
+  try {
+    loadMoreBtn.disable();
+    const images = await imagesAPIService.fetchImages();
+    renderMarkup.items = images;
+    renderMarkup.render();
+  } catch {
+    Notify.failure(error.message);
   }
-  
-  // Scroll page
-  function pageScroll() {
-    const { height: formHeight } = refs.form.getBoundingClientRect();
-    const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
-  
-    window.scrollBy({
-      top: cardHeight * 2 - formHeight * 2,
-      behavior: 'smooth',
-    });
+
+  if (imagesAPIService.endOfHits) {
+    loadMoreBtn.hideBtn();
+    return;
   }
+  loadMoreBtn.enable();
+}
+
+// Scroll page
+function pageScroll() {
+  const { height: formHeight } = refs.form.getBoundingClientRect();
+  const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2 - formHeight * 2,
+    behavior: 'smooth',
+  });
+}
